a1f30ee713b36acdf2445195d729ee6cdea8ac73
karaage0703 <coldchicken@gmail.com>
2019-12-28 05:56:36 +0000

    Add remote shutter button line

--- zangi.py
+++ zangi.py
@@ -18,15 +18,18 @@ preview_numb = 0
 
 loop = True
 
-shutter_button = 6
+power_button = 6
 preview_button = 26
+shutter_button = 5
 
 # gpio
 pi = pigpio.pi()
-pi.set_mode(shutter_button, pigpio.INPUT)
+pi.set_mode(power_button, pigpio.INPUT)
 pi.set_mode(preview_button, pigpio.INPUT)
-pi.set_pull_up_down(shutter_button, pigpio.PUD_UP)
+pi.set_mode(shutter_button, pigpio.INPUT)
+pi.set_pull_up_down(power_button, pigpio.PUD_UP)
 pi.set_pull_up_down(preview_button, pigpio.PUD_UP)
+pi.set_pull_up_down(shutter_button, pigpio.PUD_UP)
 
 shutter_state = 0
 
@@ -35,7 +38,7 @@ def go_home():
     preview_numb = sh.shutter_numb
     gui.screen_home()
 
-def cb_shutter(gpio, level, tick):
+def cb_power(gpio, level, tick):
     global shutter_state
     # print (gpio, level, tick)
     if KeepWatchForSeconds(0.5, gpio):
@@ -59,6 +62,17 @@ def cb_shutter(gpio, level, tick):
                     go_home()
                     shutter_state = 0
 
+def cb_shutter(gpio, level, tick):
+    # print (gpio, level, tick)
+    if KeepWatchForSeconds(0.5, gpio):
+        sh.camera.start_preview()
+        time.sleep(3)
+        sh.camera.stop_preview()
+        gui.screen_shutter()
+        sh.shutter()
+        time.sleep(2)
+        go_home()
+
 def cb_preview(gpio, level, tick):
     # print (gpio, level, tick)
     if KeepWatchForSeconds(0.5, gpio):
@@ -75,17 +89,17 @@ def KeepWatchForSeconds(seconds, pin_numb):
     return GoFlag
 
 def CallShutdown():
-    print("Going shutdown by GPIO.")
+    # print("Going shutdown by GPIO.")
     sh.camera.close()
     syslog.syslog(syslog.LOG_NOTICE, "Going shutdown by GPIO.")
     os.system("/sbin/shutdown -h now 'Poweroff by GPIO'")
 
 def preview():
-    print ("preview")
+    # print ("preview")
+    global preview_numb
     if preview_numb == 0:
         gui.screen_nophoto()
     else:
-        global preview_numb
         filename = os.path.join(sh.photo_dir, str("{0:06d}".format(preview_numb)) + '.jpg')
         gui.screen_preview(filename)
 
@@ -93,13 +107,15 @@ def preview():
         if preview_numb < 1:
             preview_numb = sh.shutter_numb
 
-cb1 = pi.callback(shutter_button, pigpio.FALLING_EDGE, cb_shutter)
+cb1 = pi.callback(power_button, pigpio.FALLING_EDGE, cb_power)
 cb2 = pi.callback(preview_button, pigpio.FALLING_EDGE, cb_preview)
+cb3 = pi.callback(shutter_button, pigpio.FALLING_EDGE, cb_shutter)
 
 if __name__ == '__main__':
     if not pi.connected:
         exit()
 
+    sh.setting()
     sh.loadFile()
     preview_numb = sh.shutter_numb
 
